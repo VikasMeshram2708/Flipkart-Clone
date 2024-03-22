e00bf4538907ca29679d9d4da4e8d36d2bdd3263
VikasMeshram2708 <meshramvikas744@gmail.com>
2024-03-22 13:53:01 +0530

    :white_check_mark: update code.

--- flipkart-clone/app/page.tsx
+++ flipkart-clone/app/page.tsx
@@ -2,7 +2,8 @@ import Carousel from '@/components/Carousel';
 
 export default function Home() {
   return (
-    <main className="">
+    <main className="min-h-screen max-w-[90%] mx-auto">
+      {/** TODO: Carousel */}
       <div className="mt-10">
         <Carousel />
       </div>
